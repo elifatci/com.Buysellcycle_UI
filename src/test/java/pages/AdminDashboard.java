7c7caafc98ca97946c0842ff37e0d8fb009c0f82
mithrili <altnskihsan@gmail.com>
2024-03-29 22:53:51 +0300

    ihsan/29.03/US4-10-21-30 için locate'ler eklendi

--- src/test/java/pages/AdminDashboard.java
+++ src/test/java/pages/AdminDashboard.java
@@ -1,6 +1,58 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
 public class AdminDashboard extends Base{
+    //----------------------------------
+    @FindBy(xpath = "(//li[@class])[8]")
+    public WebElement dropDownProducts;
 
+    @FindBy(xpath = "(//li/ul)[6]/li[6]")
+    public WebElement linkProductList;
 
+    @FindBy(xpath = "//*[@href='#order_complete_data']")
+    public WebElement linkSellerRequestProduct;
+
+    @FindBy(xpath = "//*[@href='#product_disabled_data']")
+    public WebElement linkDisabledProduct;
+
+    @FindBy(xpath = "//*[@href='#product_sku_data']")
+    public WebElement linkProductBySKU;
+
+    @FindBy(xpath = "(//*[@class='mb-0 mr-30 mb_xs_15px mb_sm_20px'])[1]")
+    public WebElement labelProductList;
+
+    @FindBy(xpath = "(//*[@class='mb-0 mr-30 mb_xs_15px mb_sm_20px'])[2]")
+    public WebElement labelSellerRequestProduct;
+
+    @FindBy(xpath = "(//*[@class='mb-0 mr-30 mb_xs_15px mb_sm_20px'])[3]")
+    public WebElement labelDisabledProduct;
+
+    @FindBy(xpath = "(//*[@class='mb-0 mr-30 mb_xs_15px mb_sm_20px'])[4]")
+    public WebElement labelProductBySKU;
+
+    @FindBy(xpath = " (//thead)[1]/tr[1]/th")
+    public List<WebElement> rowListProductList;
+
+    @FindBy(xpath = "(//*[@type='search'])[1]")
+    public WebElement textBoxQuickSearch;
+
+    @FindBy(xpath = "(//tbody)[1]/tr[1]/td[2]")
+    public WebElement labelSearchedProduct;
+
+    @FindBy(xpath = "(//*[@class='slider round'])[1]")
+    public WebElement radioButtonStatus;
+
+    @FindBy(xpath = "(//*[@id='dropdownMenu2'])[1]")
+    public WebElement dropDownSelect;
+
+    @FindBy(xpath = "(//*[@data-id='438'])[2]")
+    public WebElement linkViewProduct;
+
+    @FindBy(xpath = "//*[@href='https://qa.buysellcycle.com/products/438/edit ']")
+    public WebElement linkEditProduct;
+    //----------------------------------
 }
--- src/test/java/pages/UserDashboard.java
+++ src/test/java/pages/UserDashboard.java
@@ -3,6 +3,8 @@ package pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class UserDashboard extends Base{
 
     @FindBy(xpath = "(//*[@name='login'])[2]")
@@ -10,6 +12,97 @@ public class UserDashboard extends Base{
 
     @FindBy(xpath = "//*[@name='password']")
     public WebElement textBoxPassword;
+
+    //----------------------------------
+    @FindBy(xpath = "(//*[@href='https://qa.buysellcycle.com/wallet/customer/my-wallet-index'])[2]")
+    public WebElement linkMyWallet;
+
+    @FindBy(xpath = "//body/div[3]//div[2]/div/div[2]/div")
+    public List<WebElement> linkListMyWallet;
+
+    //Homepage>> LoginLink>> Email Text Box ||
+    @FindBy(xpath = "(//*[@data-bs-target='#recharge_wallet'])[2]")
+    public WebElement linkRechargeWallet;
+
+    @FindBy(xpath = "//*[@name='recharge_amount']")
+    public WebElement textBoxRechargeAmount;
+
+    @FindBy(xpath = "//*[text()='Add Fund']")
+    public WebElement addFundButton;
+
+    @FindBy(xpath = "//*[@class='wallet_elemnt active']")
+    public WebElement linkPaymentType;
+
+    @FindBy(xpath = "(//*[text()='Stripe Payment'])[2]")
+    public WebElement labelPaymentType;
+
+    @FindBy(id = "email")
+    public WebElement textBoxEmail;
+
+    @FindBy(id = "card_number")
+    public WebElement textBoxCardNumber;
+
+    @FindBy(id = "cc-exp")
+    public WebElement textBoxExpirationDate;
+
+    @FindBy(id = "cc-csc")
+    public WebElement textBoxCsc;
+
+    @FindBy(xpath = "//*[@class='iconTick']")
+    public WebElement payButton;
+
+    @FindBy(xpath = "(//thead)[1]/tr/th")
+    public List<WebElement> rowListWalletHistory;
+
+    @FindBy(xpath = "(//*[@href='https://qa.buysellcycle.com/my-wishlist'])[2]")
+    public WebElement linkMyWishlist;
+
+    @FindBy(xpath = "//*[@class='font_14 f_w_400 flex-fill mb-0']")
+    public WebElement labelResults;
+
+    @FindBy(xpath = "//*[@name='sort_by']")
+    public WebElement dropDownSortBy;
+
+    @FindBy(xpath = "//*[@name='paginate_by']")
+    public WebElement dropDownShowItems;
+
+    @FindBy(xpath = "(//*[@title='Compare'])[1]")
+    public WebElement iconCompare;
+
+    @FindBy(xpath = "(//*[@title='Compare'])[2]")
+    public WebElement iconCompare2;
+
+    @FindBy(xpath = "//*[@href='https://qa.buysellcycle.com/compare']")
+    public WebElement linkCompare;
+
+    @FindBy(xpath = "//div/div[2]/div/div[1]/div/div[1]/a")
+    public WebElement linkFirstProduct;
+
+    @FindBy(xpath = "//div/div[2]/div/div[2]/div/div[1]/a")
+    public WebElement linkSecondProduct;
+
+    @FindBy(xpath = "(//*[@title='Delete'])[1]")
+    public WebElement iconDelete;
+
+    @FindBy(xpath = "//*[text()='Delete Wishlist Product']")
+    public WebElement labelDeleteWishlist;
+
+    @FindBy(id = "dataDeleteBtn")
+    public WebElement deleteButton;
+
+    @FindBy(xpath = "(//*[@title='Add to Cart'])[1]")
+    public WebElement iconAddToCart;
+
+    @FindBy(xpath = "(//*[text()='Item added to your cart'])[1]")
+    public WebElement labelItemAddedConfirmation;
+    //----------------------------------
+
+
+
+
+
+
+
 
 
     public void loginUser(String email, String password){
