2033cb58cbb9397e09ea570dedf6a6302a4fb435
Zehra Demir <zdemir2702@gmail.com>
2024-03-29 22:55:46 +0300

    zehra/29.03/US07eklendi

--- src/test/java/pages/VisitorHomePage.java
+++ src/test/java/pages/VisitorHomePage.java
@@ -2,10 +2,122 @@ package pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import utils.ReusableMethods;
+
+import static org.junit.Assert.assertTrue;
 
 public class VisitorHomePage extends Base{
 
 
     @FindBy(linkText = "Login")
     public WebElement linkLogin;
+
+
+
+    //==============VisitorHomePage >> Footer >>US16 ==============
+    @FindBy(xpath = "//i[@class='fab fa-youtube']")
+    public WebElement iconFooterYoutube;
+
+
+    @FindBy(xpath = "//i[@class='fab fa-linkedin']")
+    public WebElement iconFooterLinkedin;
+
+
+    @FindBy(xpath = "//i[@class='fab fa-instagram']")
+    public WebElement iconFooterInstagram;
+
+    @FindBy(xpath = "//i[@class='fab fa-facebook-square']")
+    public WebElement iconFooterFacebook;
+
+    @FindBy(xpath = "//div[@class='copyright_area p-0']")
+    public WebElement labelCopyrightAll;
+
+    @FindBy(xpath = "//*[@id='subscription_email_id']")
+    public WebElement textBoxFooterEmail;
+
+    @FindBy(xpath = "//*[@id='subscribeBtn']")
+    public WebElement buttonSubscribe;
+
+    @FindBy(xpath = "//div[@class='main_footer_wrap']")
+    public WebElement footer;
+
+    @FindBy(xpath = "(//ul[@class='footer_links']/li)[1]")
+    public WebElement linkFooterAboutUs;
+
+    @FindBy(xpath = "(//ul[@class='footer_links']/li)[2]")
+    public WebElement linkFooterBlog;
+
+    @FindBy(xpath = "(//ul[@class='footer_links']/li)[3]")
+    public WebElement linkFooterDashboard;
+
+    @FindBy(xpath = "(//ul[@class='footer_links']/li)[4]")
+    public WebElement linkFooterMyProfile;
+
+    @FindBy(xpath = "(//ul[@class='footer_links']/li)[5]")
+    public WebElement linkFooterMyOrder;
+
+    @FindBy(xpath = "(//div[@class='apps_boxs']/a)[1]")
+    public WebElement buttonGooglePlayFooter;
+
+    @FindBy(xpath = "(//div[@class='apps_boxs']/a)[2]")
+    public WebElement buttonAppStoreFooter;
+
+    @FindBy(xpath = "(//div[@class='footer_list_links']/a)[1]")
+    public WebElement linkHelpContact;
+
+    @FindBy(xpath = "(//div[@class='footer_list_links']/a)[2]")
+    public WebElement linkTrackOrder;
+
+    @FindBy(xpath = "(//div[@class='footer_list_links']/a)[3]")
+    public WebElement linkReturnExchange;
+   //===================================================================
+
+
+
+    //============= New Product Deals >> Best Deals >>US07===========
+    @FindBy(xpath = "//div[contains(@class, 'd-md-block')]")
+    public WebElement dropDownShowItem;
+
+    @FindBy(xpath = "//div[@class='shorting_box']")
+    public WebElement dropDownSortingNew;
+
+    @FindBy(xpath = "//div[@class='course_category_inner']")
+    public WebElement checkBoxFilter;//Filitre kutusunun tamamı
+
+    @FindBy(xpath = "//*[@id='refresh_btn']")
+    public WebElement buttonRefresh;
+
+    @FindBy(xpath = "(//ul[@id='mobile-menu']/li)[2]")
+    public WebElement linkNewProductDeals;
+
+    @FindBy(xpath = "//h3[@class='branding_text']")
+    public WebElement labelBestDeals;
+
+    @FindBy(xpath = "(//label[@class='primary_checkbox d-flex'])[1]")
+    public WebElement checkBoxWomensApparel;//soldaki kategorilerden ilki
+
+    @FindBy(xpath = "(//*[@class='stock_text'])[2]")
+    public WebElement labelCategory;//urunun içine girildiğinde category yazısı
+    @FindBy(xpath = "(//div[@class='product__meta text-center'])[1]")
+    public WebElement linkProducktText;//gösterilen ürünlerin text kısmının ilki
+
+    @FindBy(xpath = "(//ul[@class='Check_sidebar mb_35'])[1]")
+    public WebElement checkBoxFilterProducts;//Fashion kategorisi
+
+    public void control() {
+        String expectedResult = visitorHomePage.linkProducktText.getText();
+        visitorHomePage.buttonRefresh.click();
+        ReusableMethods.wait(5);
+
+       // WebElement actualWebE = ReusableMethods.s(visitorHomePage.linkProducktText);
+        String actualResult = visitorHomePage.linkProducktText.getText();
+        assertTrue(actualResult.contains(expectedResult));
+
+    }
+    //======================================================================
+
+
+
+
+
 }
---
+++ src/test/resources/features/visitor/US07.feature
@@ -0,0 +1,50 @@
+
+  @US07
+  Feature: US07 As a user, I would like to have a page where I can follow the current products on the site where I shop.
+    Background:
+      * Go to "url"
+
+    Scenario: TC_01 When the user enters the home page, verify that the 'New Product Deals' menu title is visible in the NavBar.
+
+    * Verifies that the New Product Deals menu title is visible and active
+    * Click on the New Product Deals menu title
+    * Verifies that you are directed to the page where current product offers are listed
+    * Closes page
+
+    Scenario: TC_02 Filters for filtering operations appear in the left section of the Best Deals page and must be functional.
+
+    * Click on the New Product Deals menu title
+    * Verifies that each filter option is visible on the Best Deals page that opens.
+    * Selects and applies a filter option.
+    * After the filter is applied, it verifies that the product list is updated according to this filtering.
+    * Try different filter combinations and check that the results are updated correctly each time.
+    * Verifies that all products are listed again when the filters are reset.
+    * Closes page
+
+    Scenario: TC_03 Current products should be listed on the Best Deals page.
+
+    * Click on the New Product Deals menu title
+    * On the Best Deals page that opens, it verifies that the listing order of the products is sorted according to the newest date.
+    * Checks the up-to-date status of product information by going to the detail page of each product.
+    * By checking the products in different categories, it verifies that the most up-to-date products are listed in each category.
+    * Refresh the page and verify that the product list has been updated
+    * Closes page
+
+    Scenario: TC_04 Listing and display settings should be able to be changed while listing the products on the Best Deals page.
+
+    * Click on the New Product Deals menu title
+    * 'Show 9 item's' DDM verifies that it is visible
+    * 'Sorting by' confirms that DDM is visible
+    * Clicks on the 'Show 9 item's' DDM menu and selects one of the options in the list
+    * Verifies that the page has changed according to the selected view
+    * Clicks on the 'Sorting by' DDM menu and verifies that the new, old, name, price options are seen in the list.
+    * Verifies that the page changes correctly when one of the options is clicked
+    * Closes page
+
+      Scenario: TC_05 The refresh button on the Best Deals page should be visible and functional.
+
+      * Click on the New Product Deals menu title
+      * Verifies that the Refresh button is visible and active
+      * Clicks the Refresh button
+      * Verifies that the page has been refreshed and the content has been updated
+      * Closes page
